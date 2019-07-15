e17930d83deb57216a7fd2605b07ea5bc8e47515
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-15 16:53:34 +0800

    Refactor: optimize renderBannder function of BannerRender

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -2,10 +2,11 @@ package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
     String renderBanner(String platform, String browser) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1)) {
-            return "IE on Mac?";
-        }
-        return "banner";
+        return checkBannerValid(platform,browser)?"IE on Mac?":"banner";
+    }
+
+    private boolean checkBannerValid(String platform, String browser) {
+        return (platform.toUpperCase().indexOf("MAC") > -1) &&
+                (browser.toUpperCase().indexOf("IE") > -1);
     }
 }
