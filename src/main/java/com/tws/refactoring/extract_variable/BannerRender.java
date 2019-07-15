485049afab60e9365bc4ee88300427bc31b6ac87
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-15 17:33:51 +0800

    Refactor: extract variable of renderBanner

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -1,12 +1,15 @@
 package com.tws.refactoring.extract_variable;
 
 public class BannerRender {
+    private static final String MAC = "MAC";
+    private static final String IE = "IE";
     String renderBanner(String platform, String browser) {
         return checkBannerValid(platform,browser)?"IE on Mac?":"banner";
     }
 
     private boolean checkBannerValid(String platform, String browser) {
-        return (platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1);
+
+        return (platform.toUpperCase().indexOf(MAC) > -1) &&
+                (browser.toUpperCase().indexOf(IE) > -1);
     }
 }
