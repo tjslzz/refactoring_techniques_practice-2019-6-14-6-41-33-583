311881d3191d12996fcfde3669489962e84c7060
JERRY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIJE9@OOCL.COM>
2019-07-15 17:32:40 +0800

    Refactor: extract variable of getPrice

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,10 +1,15 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+    private static final int QUANTITY_BOUND = 500;
+    private static final double FIVE_DISCOUNT = 0.05;
+    private static final double MIN_COST = 100.0;
+    private static final double ONE_DISCOUNT = 0.1;
+
     double getPrice(int quantity, int itemPrice) {
-        // Price consists of: base price - discount + shipping cost
-        return quantity * itemPrice -
-                Math.max(0, quantity - 500) * itemPrice * 0.05 +
-                Math.min(quantity * itemPrice * 0.1, 100.0);
+        int totalPrice = quantity * itemPrice;
+        double discount = Math.max(0, quantity - QUANTITY_BOUND) * itemPrice * FIVE_DISCOUNT;
+        double shippingCost = Math.min(quantity * itemPrice * ONE_DISCOUNT, MIN_COST);
+        return totalPrice - discount + shippingCost;
     }
 }
